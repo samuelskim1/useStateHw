b3ee75e9c25e8f2c2a3eab43e1f494a9047a90e7
samuelskim1 <116222606+samuelskim1@users.noreply.github.com>
2023-03-20 01:12:09 -0400

    Initial commit

---
+++ src/components/UseState/index.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./UseState.css";
+
+const UseState = () => {
+  console.log(useState("light"));
+  return (
+    <div className="state">
+      <h1>UseState Component</h1>
+      <button>Dark</button>
+      <button>Light</button>
+      <h2>DISPLAY COUNT HERE</h2>
+      <button>Increment</button>
+      <button>Decrement</button>
+    </div>
+  );
+};
+
+export default UseState;
